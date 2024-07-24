7a7d77b296d7f585c17cec1b007a34fb2f2ac5e1
shubham2o <2contactshubham@gmail.com>
2024-07-24 22:48:46 +0530

    minor bugs in layout, design - fixed

--- Fake-Store/src/components/Loading.jsx
+++ Fake-Store/src/components/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
-        <div className="w-[85%] h-full flex justify-center items-center">
-            <h1 className="text-3xl tracking-wide">Loading...</h1>
+        <div className="w-[81.5%] h-full flex justify-center items-center">
+            <h1 className="text-5xl tracking-wider">Loading...</h1>
         </div>
     )
 }
--- Fake-Store/src/components/Sidebar.jsx
+++ Fake-Store/src/components/Sidebar.jsx
@@ -1,32 +1,32 @@
 const Sidebar = () => {
     return (
-        <nav className="w-[15%] h-full pt-6 bg-zinc-700 flex flex-col items-center">
+        <nav className="w-[18.5%] h-full pt-6 bg-zinc-700 flex flex-col items-center">
             <a
                 href="/create"
-                className="border-2 border-blue-400 rounded-lg px-3 py-1.5 text-blue-400 font-bold tracking-wide"
+                className="border-2 border-blue-400 rounded-xl px-6 py-3 text-blue-400 text-lg font-semibold tracking-wider"
             >
                 Add New product
             </a>
 
             <hr className="w-[80%] my-4" />
 
-            <h1 className="w-[80%] mb-3 text-xl font-semibold">
+            <h1 className="w-[80%] mb-3 text-2xl font-semibold">
                 Category Filter
             </h1>
 
-            <ul className="w-[80%] text-sm tracking-wide">
+            <ul className="w-[80%] tracking-wide">
                 <li className="mb-3 flex items-center gap-2.5">
-                    <span className="w-[9px] h-[9px] mb-0.5 rounded-full bg-red-500"></span>
+                    <span className="w-[10px] h-[10px] mb-0.5 rounded-full bg-red-500"></span>
                     Category 1
                 </li>
 
                 <li className="mb-3 flex items-center gap-2.5">
-                    <span className="w-[9px] h-[9px] mb-0.5 rounded-full bg-yellow-500"></span>
+                    <span className="w-[10px] h-[10px] mb-0.5 rounded-full bg-yellow-500"></span>
                     Category 2
                 </li>
 
                 <li className="mb-3 flex items-center gap-2.5">
-                    <span className="w-[9px] h-[9px] mb-0.5 rounded-full bg-green-500"></span>
+                    <span className="w-[10px] h-[10px] mb-0.5 rounded-full bg-green-500"></span>
                     Category 3
                 </li>
             </ul>
